312032bbcdbeeffb796e88f7d06e7d1a68823476
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2022-11-03 18:02:42 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/get',(req,res)=>{
+    res.send('<h1>Hello World</h1>')
+})
+
+
+app.listen(5000,()=>{
+    console.log(`Listening on Port 5000`)
+})
